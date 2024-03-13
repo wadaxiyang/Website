3039282b840f6ce01163117fab24498dabf6e413
wadaxiyang <wadaxiyang_@outlook.com>
2024-03-13 12:52:05 +0800

    repair-6

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -2,8 +2,8 @@ import { defineConfig } from 'vitepress'
 import { set_sidebar } from "./utils/auto_sidebar.js";	// 改成自己的路径
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  // base: "/Website/",
-  head: [["link", { rel: "icon", href: "bilibili.svg" }]],
+  base: "/Website/",
+  head: [["link", { rel: "icon", href: "/Website/Blog.svg" }]],
   title: "wadaxiyangのBLOG",
   description: "Personal Blog",
   themeConfig: {
